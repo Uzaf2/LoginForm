09c3ad4ea5cbd0892086f4eb6486e396f5d3ee2a
Usman Zafar <Uzman.Zafar1@gmail.com>
2021-01-16 17:35:21 +1100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import  Form  from "react-bootstrap/Form";
+import  Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
+  var style = {
+    color: "#FFFAF0",
+    background: "#2F4F4F",
+    height: "70px",
+    width: "100%"
+  };
+
+  var style2 = {
+    color: "white",
+    background: "#000000",
+    height: "50px"
+  };
+  
   return (
+    
+    <div className="main">
+       <Navbar bg="dark" variant="dark" style={style2}></Navbar>
+       <p className="heading"> Social Media Application </p>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="div1">
+      <div class="formDiv1">
+     <input type="text" class= "name" name="name" placeholder="Your name" required/>
+     <input type="text" class="email" name="email" placeholder="Your Email" required/>
+     <input type="password" class="password" name="password" placeholder="Password" required/>
+        <input type="password" class= "password" name="confirmPassword" placeholder="Confirm Password" required/>
+        <input type="submit" class="btn1" value="SIGN UP"/> 
+      </div>
+      </div>
+
+      <div class="div2">
+        <div class="formDiv2">
+          <p className="LoginHeading">Login</p>
+     <input type="text" class= "email" name="email" placeholder="Your Email" required/>
+     <input type="password" class="password" name="password" placeholder="Password" required/>
+        <input type="submit" class="btn2" value="LOGIN"/> 
+      </div>
+      </div>
+
+
+    </div>
     </div>
   );
 }
